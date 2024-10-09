08800f2f0c18adc04285e176cba7facee219816c
AGX <agxnh2020@gmail.com>
2024-10-09 16:50:04 +0300

    added animal and breed to SearchParams

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
   return (
     <div className="search-params">
       <form>
@@ -13,6 +18,38 @@ const SearchParams = () => {
             value={location}
             placeholder="Location"
           />
+        </label>
+
+        <label htmlFor="animal">
+          Animal
+          <select
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            id="animal"
+            value={animal}
+          >
+            <option value=""></option>
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+
+        <label htmlFor="breed">
+          Breed
+          <select
+            onChange={(e) => setBreed(e.target.value)}
+            id="breed"
+            value={breed}
+            disabled={breeds.length === 0}
+          >
+            <option value=""></option>
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
