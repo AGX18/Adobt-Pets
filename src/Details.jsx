c56de364bb48b196491ce22b8b80dd33dc820b7a
AGX <agxnh2020@gmail.com>
2024-10-12 03:18:53 +0300

    refactored code to use react query instead and removed all useEffect code and added Carousel class

---
+++ src/Details.jsx
@@ -0,0 +1,37 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import Carousel from "./Carousel";
+import fetchPet from "./fetchPet";
+
+const Details = () => {
+  const { id } = useParams();
+  const results = useQuery(["details", id], fetchPet);
+
+  if (results.isError) {
+    return <h2>Oh No!!</h2>;
+  }
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+
+  return (
+    <div className="details">
+      <Carousel images={pet.images}></Carousel>
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>{`${pet.animal} — ${pet.breed} — ${pet.city}, ${pet.state}`}</h2>
+        <button>Adopt {pet.name}</button>
+        <p>{pet.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Details;
